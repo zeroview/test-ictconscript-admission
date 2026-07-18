f8ad427041b4c270285481c8f49fe9b641f559a3
zeroview <zeroviewzero@gmail.com>
2026-07-18 21:20:12 +0300

    feat: add map thumbnails

--- src/lib/components/LogEntry.svelte
+++ src/lib/components/LogEntry.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { type LogEntryData } from "$lib/data";
-  import { ChevronDownIcon } from "@lucide/svelte";
+  import { ChevronDownIcon, MapPinIcon } from "@lucide/svelte";
   import { onMount } from "svelte";
+  import { MapLibre, Marker } from "svelte-maplibre-gl";
   let {
     logEntry,
     expanded,
@@ -23,6 +24,10 @@
     contentHeight = content?.scrollHeight ?? 0;
   };
   onMount(updateContentHeight);
+
+  let lnglat = $derived(
+    logEntry.lon !== null && logEntry.lat !== null ? { lng: logEntry.lon, lat: logEntry.lat } : null
+  );
 </script>
 
 <svelte:window onresize={updateContentHeight} />
@@ -41,10 +46,39 @@
     </button>
     <div
       bind:this={content}
-      class={"overflow-hidden w-fit transition-[max-height]"}
+      class={"overflow-hidden w-full transition-[max-height] grid grid-cols-[1fr_12rem]"}
       style={"max-height: " + (expanded ? contentHeight : 0) + "px;"}
     >
       <p class="whitespace-pre-line">{logEntry.body}</p>
+      {#if lnglat}
+        <MapLibre
+          class="aspect-square size-full rounded-md cursor-pointer"
+          zoom={12}
+          style={{
+            version: 8,
+            sources: {
+              "raster-tiles": {
+                type: "raster",
+                tiles: [
+                  "https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+                  "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+                  "https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+                  "https://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                ],
+                tileSize: 256
+              }
+            },
+            layers: [{ id: "raster-layer", type: "raster", source: "raster-tiles" }]
+          }}
+          center={lnglat}
+          interactive={false}
+        >
+          <Marker {lnglat} />
+        </MapLibre>
+      {/if}
     </div>
   </div>
 </div>
+
+<style>
+</style>
--- src/lib/components/NewEntryModal.svelte
+++ src/lib/components/NewEntryModal.svelte
@@ -15,6 +15,8 @@
   let timeInput: HTMLInputElement | undefined = $state();
   let title = $state("");
   let body = $state("");
+  let lat = $state(null);
+  let lon = $state(null);
 
   let canBeSubmitted = $derived(title !== "");
 
@@ -26,8 +28,8 @@
       title,
       body,
       isoTime: new Date(`${dateInput.value}T${timeInput.value}`).toISOString(),
-      lat: null,
-      lon: null
+      lat,
+      lon
     };
     console.log(data);
     onsubmit(data);
@@ -48,13 +50,15 @@
   });
 </script>
 
-<div class="mx-8 sm:mx-16 w-2xl relative p-4 bg-green-50/100 h-fit rounded-2xl">
+<div class="mx-8 sm:mx-16 w-2xl relative p-4 bg-green-50 h-fit rounded-2xl">
   <h2 class="font-bold mb-4 text-2xl text-center">New log entry</h2>
+
   <form class="grid grid-cols-[6rem_1fr] gap-y-3">
     <label for="date" class="font-semibold">Date:</label>
     <input type="text" id="date" class="cursor-pointer" bind:this={dateInput} />
     <label for="time" class="font-semibold">Time:</label>
     <input type="text" id="time" class="cursor-pointer" bind:this={timeInput} />
+
     <label for="title" class="font-semibold col-span-2 xs:col-span-1">Title:</label>
     <input
       type="text"
@@ -69,6 +73,27 @@
       rows="6"
       class="rounded-md px-2 border-2 border-neutral-400 resize-none col-span-2 xs:col-span-1"
     ></textarea>
+
+    <label for="location" class="font-bold col-span-2 xs:col-span-1">Location:</label>
+    <div class="flex gap-2" id="location">
+      <label for="lat">Latitude:</label>
+      <input
+        type="number"
+        step="any"
+        id="lat"
+        class="rounded-md w-30 px-2 border-2 border-neutral-400 col-span-2 xs:col-span-1"
+        bind:value={lat}
+      />
+      <label for="lon" class="ml-10">Longitude:</label>
+      <input
+        type="number"
+        step="any"
+        id="lon"
+        class="rounded-md w-30 px-2 border-2 border-neutral-400 col-span-2 xs:col-span-1"
+        bind:value={lon}
+      />
+    </div>
+
     <div class="flex w-full col-span-2 gap-2 justify-end">
       <button
         class="rounded-md px-2 py-1 border-2 border-neutral-500 disabled:border-neutral-300 disabled:text-gray-400"
@@ -81,6 +106,7 @@
       >
     </div>
   </form>
+
   <button class="absolute top-4 right-4" onclick={onclose}>
     <XIcon class="size-5 transition-transform hover:scale-110" />
   </button>
