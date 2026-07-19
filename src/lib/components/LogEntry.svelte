3980a466cb16ab0e06a0a267fc7a1fc4dfc344d7
zeroview <zeroviewzero@gmail.com>
2026-07-19 21:09:11 +0300

    feat: better log entry layout

--- src/lib/components/LogEntry.svelte
+++ src/lib/components/LogEntry.svelte
@@ -36,55 +36,53 @@
 <div
   class={"rounded-lg mb-4 relative transition-[height] duration-300 border-2 border-neutral-400 bg-green-50/80 backdrop-blur-sm"}
 >
-  <div class="mx-4 my-3">
-    <button class="flex w-full justify-between group" onclick={toggleExpansion}>
-      <h3 class="w-fit text-start font-bold text-xl">
-        {entryTime + " - " + logEntry.title}
-      </h3>
-      <ChevronDownIcon
-        class={`transition-transform h-fit duration-300 ${expanded ? "rotate-180 group-hover:-translate-y-1" : "group-hover:translate-y-1"}`}
-      />
-    </button>
-    <div
-      bind:this={content}
-      class={"overflow-hidden w-full transition-[max-height] grid gap-4 sm:grid-cols-[1fr_16rem] grid-cols-1"}
-      style={"max-height: " + (expanded ? contentHeight : 0) + "px;"}
-    >
-      <p class="whitespace-pre-line">{logEntry.body}</p>
-      {#if lnglat}
-        <div class="sm:aspect-square relative w-full sm:h-full h-50">
-          <MapLibre
-            class="size-full rounded-lg cursor-pointer"
-            zoom={12}
-            style={{
-              version: 8,
-              sources: {
-                "raster-tiles": {
-                  type: "raster",
-                  tiles: [
-                    "https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-                    "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-                    "https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-                    "https://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
-                  ],
-                  tileSize: 256
-                }
-              },
-              layers: [{ id: "raster-layer", type: "raster", source: "raster-tiles" }]
-            }}
-            center={lnglat}
-            interactive={false}
-          >
-            <Marker {lnglat}></Marker>
-          </MapLibre>
-          <p
-            class="whitespace-pre-line absolute bottom-0 drop-shadow-xs text-center rounded-b-lg font-mono w-full bg-green-50/70"
-          >
-            {`${formatDMS(lnglat.lat, "lat")} ${formatDMS(lnglat.lng, "lon")}`}
-          </p>
-        </div>
-      {/if}
-    </div>
+  <button class="flex w-full justify-between group px-4 py-3" onclick={toggleExpansion}>
+    <h3 class="w-fit text-start font-bold text-xl">
+      {entryTime + " - " + logEntry.title}
+    </h3>
+    <ChevronDownIcon
+      class={`transition-transform h-fit duration-300 ${expanded ? "rotate-180 group-hover:-translate-y-1" : "group-hover:translate-y-1"}`}
+    />
+  </button>
+  <div
+    bind:this={content}
+    class={`overflow-y-hidden transition-[max-height_margin-bottom] grid gap-4 sm:grid-cols-[1fr_16rem] grid-cols-1 px-4`}
+    style={`max-height: ${expanded ? contentHeight : 0}px; margin-bottom: ${expanded ? 1 : 0}rem;`}
+  >
+    <p class="whitespace-pre-line">{logEntry.body}</p>
+    {#if lnglat}
+      <div class="sm:aspect-square relative w-full sm:h-full h-50">
+        <MapLibre
+          class="size-full rounded-lg cursor-pointer"
+          zoom={12}
+          style={{
+            version: 8,
+            sources: {
+              "raster-tiles": {
+                type: "raster",
+                tiles: [
+                  "https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+                  "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+                  "https://mt2.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+                  "https://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                ],
+                tileSize: 256
+              }
+            },
+            layers: [{ id: "raster-layer", type: "raster", source: "raster-tiles" }]
+          }}
+          center={lnglat}
+          interactive={false}
+        >
+          <Marker {lnglat}></Marker>
+        </MapLibre>
+        <p
+          class="whitespace-pre-line absolute bottom-0 drop-shadow-xs text-center rounded-b-lg font-mono w-full bg-green-50/70"
+        >
+          {`${formatDMS(lnglat.lat, "lat")} ${formatDMS(lnglat.lng, "lon")}`}
+        </p>
+      </div>
+    {/if}
   </div>
 </div>
 
