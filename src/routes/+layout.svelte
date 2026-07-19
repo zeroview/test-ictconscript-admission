7679b74af5be85235a9b61ed74749cb3a390870a
zeroview <zeroviewzero@gmail.com>
2026-07-19 20:00:51 +0300

    fix: fix background sizing on safari

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,6 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="h-dvh bg-[url(/m05.webp)] fixed bg-cover top-0 left-0 right-0 bottom-0 -z-10"></div>
+<div class="bg-[url(/m05.webp)] fixed bg-cover top-0 left-0 right-0 bottom-0 -z-10"></div>
 
 {@render children()}
