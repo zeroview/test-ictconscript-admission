fb13b2797c9679b715bd38cf9d01479b76ee9ceb
zeroview <zeroviewzero@gmail.com>
2026-07-19 19:36:38 +0300

    chore: setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,10 @@ export default defineConfig({
         runes: ({ filename }) =>
           filename.split(/[/\\]/).includes("node_modules") ? undefined : true
       },
-      adapter: adapter()
+      adapter: adapter(),
+      paths: {
+        base: process.argv.includes("dev") ? "" : `/${process.env.BASE_PATH}`
+      }
     })
   ],
   server: {
